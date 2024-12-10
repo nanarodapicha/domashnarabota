ba833d352edfe5224085a68824d623dc3ebf9092
User <User@DESKTOP-221P20N>
2024-12-10 20:53:31 +0200

    Add project files.

---
+++ domashnarabota/Point.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domashnarabota
+{
+    public class Point<T>
+    {
+        private T x, y;
+
+        public T X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public T Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public Point(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        //zad1
+        public void Swap()
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+        //zad2
+        public double DistanceTo(Point<T> vtori)
+        {
+            double x1 = Convert.ToDouble(this.x);
+            double y1 = Convert.ToDouble(this.y);
+            double x2 = Convert.ToDouble(vtori.X);
+            double y2 = Convert.ToDouble(vtori.Y);
+
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+        //zad3
+        public static Point<double> Midpoint(Point<T> p1, Point<T> p2)
+        {
+            double x1 = Convert.ToDouble(p1.X);
+            double y1 = Convert.ToDouble(p1.Y);
+            double x2 = Convert.ToDouble(p2.X);
+            double y2 = Convert.ToDouble(p2.Y);
+            double sredaX = (x1 + x2) / 2;
+            double sredaY = (y1 + y2) / 2;
+
+            return new Point<double>(sredaX, sredaY);
+        }
+    }
+}
---
+++ domashnarabota/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domashnarabota
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //zad1
+            Point<int> point = new Point<int>(3, 4);
+            Console.WriteLine($"normalni:\nx = {point.X}\ny = {point.Y}");
+            point.Swap();
+            Console.WriteLine($"razmeneni:\nx = {point.X}\ny = {point.Y}");
+
+
+            //zad2
+            Point<double> point1 = new Point<double>(1.0, 2.0);
+            Point<double> point2 = new Point<double>(4.0, 6.0);
+            double razstoqnie = point1.DistanceTo(point2);
+            Console.WriteLine($"Razstoqnieto mejdu tochkite ({point1.X} {point1.Y}) i ({point2.X} {point2.Y}) e {razstoqnie}");
+
+
+            //zad3
+            Point<int> point3 = new Point<int>(2, 4);
+            Point<int> point4 = new Point<int>(6, 8);
+            Point<double> srednaTochka = Point<int>.Midpoint(point3, point4);
+
+            Console.WriteLine($"Srednata tochka mejdu ({point3.X},{point4.Y}) i ({point4.X},{point4.Y}) e ({srednaTochka.X},{srednaTochka.Y})");
+        }
+    }
+}
